8629e3c74ad74e73e98de2373fcbfb67bb06354a
gcaaa32928 <gcaaa31928@gmail.com>
2018-01-17 22:17:36 +0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const http = require('http')
 const port = process.env.PORT || 3000
 
-const requestHandler = (request, response) => {
-	console.log(request.url)
+const requestHandler = (req, response) => {
+	console.dir(req.param);
+
+	if (req.method == 'POST') {
+		console.log("POST");
+		var body = '';
+		req.on('data', function (data) {
+			body += data;
+			console.log("Partial body: " + body);
+		});
+		req.on('end', function () {
+			console.log("Body: " + body);
+		});
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('post received');
+	}
 	response.end('Hello Node.js Server!')
 }
 
