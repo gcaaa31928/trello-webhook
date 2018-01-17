6ca588886081b2428de957d4c85d458d2f16ea7d
redhuang <redhuang@synology.com>
2018-01-17 18:29:30 +0800

    test

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var Trello = require("node-trello");
-var t = new Trello("5d3b5f469f111435b56e0d84a7570bf8", "d266ca014d70a0f4a9283ca2d58819090134bcdfc6baa8e731e138b547e5db9e");
+const http = require('http')
+const port = 80
 
-t.get("/1/boards/5a5f03328066cc1a059790ef/cards", function(err, data) {
-	if (err) throw err;
-	data.forEach(function(card) {
-		console.log(card);
-	});
-});
+const requestHandler = (request, response) => {
+	  console.log(request.url)
+	  response.end('Hello Node.js Server!')
+}
 
+const server = http.createServer(requestHandler)
 
-// URL arguments are passed in as an object.
-// t.get("/1/members/me", { cards: "open" }, function(err, data) {
-//   if (err) throw err;
-//     console.log(data);
-//     });
+server.listen(port, (err) => {
+	  if (err) {
+		      return console.log('something bad happened', err)
+		    }
+
+	  console.log(`server is listening on ${port}`)
+})
