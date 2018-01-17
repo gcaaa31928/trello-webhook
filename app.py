5f1e0f9342a83ea9fec6707c8c15175e7ebac1b9
gcaaa32928 <gcaaa31928@gmail.com>
2018-01-18 01:43:13 +0800

    test

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ import urllib
 import requests
 from flask import Flask, request, abort
 from flask_cors import CORS
-
+import sys
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=['POST'])
 def callback():
-    body = request.get_data(as_text=True)
-    print(body)
+    try:
+        body = request.get_data(as_text=True)
+        action = body['action']['data']
+        if action['listAfter']['name'] == '完成':
+            print('{} 已經完成 {}'.format(body['action']['display']['entities']['memberCreator']['text'], action['card']['name'])
+
+        print(body)
+    except:
+        print(sys.exc_info()[0])
     return 'OK'
 
 if __name__ == "__main__":
